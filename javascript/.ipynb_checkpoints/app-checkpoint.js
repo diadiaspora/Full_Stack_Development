24350b30d95a4b7ee6c1dab37325cbec5d54a39e
diadiaspora <dia.diaspora@gmail.com>
2026-01-06 17:16:27 -0300

    console log tests

--- javascript/.ipynb_checkpoints/app-checkpoint.js
+++ javascript/.ipynb_checkpoints/app-checkpoint.js
@@ -1,1 +1,9 @@
 console.log("JS file recognized");
+
+console.log("JS file recognized");
+
+let name = "Dia";
+console.log("Hello", name);
+
+let numbers = [1, 2, 3];
+console.log("Numbers:", numbers);
