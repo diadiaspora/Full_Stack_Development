2a16b75b6dc7c7421047fe56f2a5682e7745f724
diadiaspora <dia.diaspora@gmail.com>
2026-04-14 12:52:36 -0400

    includes method fail

---
+++ 1_HTML_CSS_Javacript/javascript/0_Ground Zero/(includes)method/app.js
@@ -0,0 +1,155 @@
+/*
+Exercise 1: containsLetterA()
+
+Create a function named containsLetterA.
+It should take a single string as an argument.
+Return true if the string contains the letter "a".
+Otherwise, return false.
+*/
+
+const containsLetterA = (str) => {
+
+}
+
+console.log('Exercise 1 Result:', containsLetterA("apple"));
+
+/*
+Exercise 2: containsWordJavaScript()
+
+Create a function named containsWordJavaScript.
+It should take a string as an argument.
+Return true if the string contains the word "JavaScript".
+Otherwise, return false.
+*/
+
+const containsWordJavaScript = (str) => {
+
+}
+
+console.log('Exercise 2 Result:', containsWordJavaScript("I love JavaScript"));
+
+/*
+Exercise 3: isCharAVowel()
+
+Create a function named isCharAVowel.
+It should take a single character as an argument.
+Return true if it is a vowel (a, e, i, o, u).
+Return false otherwise.
+*/
+
+const isCharAVowel = (char) => {
+
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("e"));
+
+/*
+Exercise 4: hasNumber()
+
+Create a function named hasNumber.
+It should take a string as an argument.
+Return true if the string contains any number (0–9).
+Otherwise, return false.
+*/
+
+const hasNumber = (str) => {
+
+}
+
+console.log('Exercise 4 Result:', hasNumber("abc3def"));
+
+/*
+Exercise 5: hasSpace()
+
+Create a function named hasSpace.
+It should take a string as an argument.
+Return true if the string contains a space.
+Otherwise, return false.
+*/
+
+const hasSpace = (str) => {
+
+}
+
+console.log('Exercise 5 Result:', hasSpace("hello world"));
+
+/*
+Exercise 6: hasBanana()
+
+Create a function named hasBanana.
+It should take an array of strings as an argument.
+Return true if the array contains "banana".
+Otherwise, return false.
+*/
+
+const hasBanana = (arr) => {
+
+}
+
+console.log('Exercise 6 Result:', hasBanana(["apple", "banana", "orange"]));
+
+/*
+Exercise 7: hasSpecialCharacter()
+
+Create a function named hasSpecialCharacter.
+It should take a string as an argument.
+Return true if the string contains any of these characters: !@#$%^&*
+Otherwise, return false.
+*/
+
+const hasSpecialCharacter = (str) => {
+
+}
+
+console.log('Exercise 7 Result:', hasSpecialCharacter("hello!"));
+
+/*
+Exercise 8: containsBannedWord()
+
+Create a function named containsBannedWord.
+It should take a string as an argument.
+Return true if the string contains any of these words:
+"bad", "hate", "angry"
+Otherwise, return false.
+*/
+
+const containsBannedWord = (str) => {
+
+}
+
+console.log('Exercise 8 Result:', containsBannedWord("I am angry"));
+
+/*
+Exercise 9: hasVowelInWord()
+
+Create a function named hasVowelInWord.
+It should take a string as an argument.
+Return true if the word contains at least one vowel.
+Otherwise, return false.
+*/
+
+const hasVowelInWord = (word) => {
+
+}
+
+console.log('Exercise 9 Result:', hasVowelInWord("sky"));
+
+/*
+Exercise 10: haveCommonLetter()
+
+Create a function named haveCommonLetter.
+It should take two strings as arguments.
+Return true if the two strings share at least one letter.
+Otherwise, return false.
+*/
+
+const haveCommonLetter = (str1, str2) => {
+
+}
+
+console.log('Exercise 10 Result:', haveCommonLetter("cat", "car"));
+
+
+
+
+
---
+++ 1_HTML_CSS_Javacript/javascript/Functions Lab/April 14/app.js
@@ -0,0 +1,87 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+//this is written as a arrow function  because it starts with const funstion name = () => {}
+
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+// as an expression:
+
+const maxoftwo = function(x,y) {
+      if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+// as a basic function :
+
+function maxofTwo(x,y) {
+    if (x >= y) {
+        return x;
+    } else {
+        return y;
+    }
+    
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+console.log('Exercise 1 Expression:', maxoftwo(3, 9));
+console.log('Exercise 1 basic:', maxofTwo(1, 1));
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+const isAdult = (x) => {
+    if (x >= 18 ) {
+        return "Adult"
+    } else {
+        return "minor"
+    }
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (char) => {
+  const vowels = 'aeiou';
+  return vowels.includes(char.toLowerCase());
+};
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
