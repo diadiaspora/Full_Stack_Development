ae7a32e1d43a958896dd49d409d177b28fb54c8d
diadiaspora <dia.diaspora@gmail.com>
2026-06-19 17:41:03 -0500

    const and vvariables

---
+++ 1_HTML_CSS_Javacript/javascript/11_DOM/DOM Events/Calculator/Calculator 3.0/app.js
@@ -0,0 +1,25 @@
+// constants________________________________________________
+
+const numbers = document.querySelectorAll('.number');
+
+const operators = document.querySelectorAll('.operator');
+
+const equalButton = document.querySelector('.equals');
+
+const clearButton = document.querySelector('.clear');
+
+const calculator = document.querySelector('.calculator');
+
+const display = document.querySelector('.display');
+
+/*-------------------------------- Variables --------------------------------*/
+
+let num1 = '';
+let num2 = '';
+let oper = '';
+let result = null;
+let clear = '';
+
+/*----------------------------- Event Listeners -----------------------------*/
+/*-------------------------------- Functions --------------------------------*/
+
