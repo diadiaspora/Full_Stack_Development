8bd5f6b0592a4354c3ab57ff43b434efd4fef97b
diadiaspora <dia.diaspora@gmail.com>
2026-06-20 08:46:24 -0500

    calc functions

--- 1_HTML_CSS_Javacript/javascript/11_DOM/DOM Events/Calculator/Calculator 3.0/app.js
+++ 1_HTML_CSS_Javacript/javascript/11_DOM/DOM Events/Calculator/Calculator 3.0/app.js
@@ -1,25 +1,57 @@
 // constants________________________________________________
 
-const numbers = document.querySelectorAll('.number');
+const numbers = document.querySelectorAll(".number");
 
-const operators = document.querySelectorAll('.operator');
+const operators = document.querySelectorAll(".operator");
 
-const equalButton = document.querySelector('.equals');
+const equalButton = document.querySelector(".equals");
 
-const clearButton = document.querySelector('.clear');
+const clearButton = document.querySelector(".clear");
 
-const calculator = document.querySelector('.calculator');
+const calculator = document.querySelector(".calculator");
 
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
 /*-------------------------------- Variables --------------------------------*/
 
-let num1 = '';
-let num2 = '';
-let oper = '';
+let num1 = "";
+let num2 = "";
+let oper = "";
 let result = null;
-let clear = '';
+let clear = "";
 
 /*----------------------------- Event Listeners -----------------------------*/
-/*-------------------------------- Functions --------------------------------*/
 
+/*-------------------------------- Functions --------------------------------*/
+init();
+
+function init() {
+  num1 = "";
+  num2 = "";
+  oper = "";
+  result = null;
+  clear = "";
+  display.innerText = "0";
+}
+
+function operate() {
+  const nums1 = parseFloat(num1);
+  const nums2 = parseFloat(num2);
+
+  switch (oper) {
+    case "+":
+      result = nums1 + nums2;
+      break;
+    case "-":
+      result = nums1 - nums2;
+      break;
+    case "*":
+      result = nums1 * nums2;
+      break;
+    case "+":
+      result = nums1 * nums2;
+      break;
+  }
+
+  display.innerText = result;
+}
