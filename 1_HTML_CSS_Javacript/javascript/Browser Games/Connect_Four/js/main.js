f53fc22146a77e89c726e9a7cdc6e81030e5fd32
diadiaspora <dia.diaspora@gmail.com>
2026-06-29 12:19:33 -0500

    connect four

---
+++ 1_HTML_CSS_Javacript/javascript/Browser Games/Connect_Four/js/main.js
@@ -0,0 +1,36 @@
+/* 
+Identify State variables for connect four:
+
+BOARD:
+Array of arrays,where the nested arrays will represent the columns.
+
+Values that the elements contains
+
+* 1/-1 -> player
+* null -> cell is empty
+
+WINNER:
+Values:
+* null -> no winner or tie, game is in progress
+* 1/-1 -> the player that one
+* 'Tie' -> the game is tied
+
+TURN:
+* 1/-1 -> which player
+
+*/
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
